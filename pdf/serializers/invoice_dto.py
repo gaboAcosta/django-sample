72c23a5332bc391f2ac2c8d984512671d3365b57
Gabo Acosta <gabo.acosta624@gmail.com>
2024-02-16 14:20:28 -0700

    Adds line items serializer

--- pdf/serializers/invoice_dto.py
+++ pdf/serializers/invoice_dto.py
@@ -3,8 +3,14 @@ from rest_framework import serializers
 from datetime import date
 
 
+class LineItemDTO(serializers.Serializer):
+    quantity = serializers.FloatField()
+    price = serializers.FloatField()
+
+
 class InvoiceDTO(serializers.Serializer):
     date = serializers.DateField(default=date.today().strftime("%d/%m/%y"))
     amount = serializers.FloatField(default=0)
     customer_name = serializers.CharField(max_length=200)
     invoice_number = serializers.IntegerField()
+    line_items = LineItemDTO(many=True)
