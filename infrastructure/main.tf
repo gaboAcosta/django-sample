16eb2d33e2b921d4495ca04cf2107fee4ecbcdb4
Gabriel Acosta <gabo.acosta624@gmail.com>
2024-02-22 08:41:24 -0700

    Adds terraform for cloud run and build actions (#3)
    * Adds terraform for cloud run and build actions
    
    * Defaults to sqlite DB

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,28 +12,53 @@ terraform {
 }
 
 provider "google" {
-  project     = var.project
-  region      = var.region
-  zone        = var.zone
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
+# [START cloudrun_service]
+resource "google_cloud_run_v2_service" "default" {
+  location = "us-central1"
+  name     = "django-test-service"
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-  tags         = ["web", "dev"]
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
     }
   }
 }
+# [END cloudrun_service_tasks_service]
+
+# [START cloudrun_service_tasks_sa]
+resource "google_service_account" "default" {
+  account_id   = "cloud-run-task-invoker"
+  display_name = "Cloud Run Task Invoker"
+}
+# [END cloudrun_service_tasks_sa]
+
+# [START cloudrun_service_tasks_run_invoke_permissions]
+resource "google_cloud_run_service_iam_binding" "default" {
+  location = google_cloud_run_v2_service.default.location
+  service  = google_cloud_run_v2_service.default.name
+  role     = "roles/run.invoker"
+  members  = ["allUsers"]
+}
+# [END cloudrun_service_tasks_run_invoke_permissions]
+
+# [START cloudrun_service_tasks_token_permissions]
+resource "google_project_iam_binding" "project_binding" {
+  project = google_cloud_run_v2_service.default.project
+  role    = "roles/iam.serviceAccountTokenCreator"
+  members = ["serviceAccount:${google_service_account.default.email}"]
+}
+# [END cloudrun_service_tasks_token_permissions]
+
+# [START artifact_registry_repository]
+resource "google_artifact_registry_repository" "my-repo" {
+  location      = "us-central1"
+  repository_id = "django-test-service-repository"
+  description   = "Django app images repository"
+  format        = "DOCKER"
+}
+# [END artifact_registry_repository]
